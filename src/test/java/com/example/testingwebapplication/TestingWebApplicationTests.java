bc87778c5f46cb5a214f683734c28bd10fafe7f6
Colin Smith <colin.kenjiro.smith@gmail.com>
2024-02-14 14:13:07 -0500

    initial commit

---
+++ src/test/java/com/example/testingwebapplication/TestingWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testingwebapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
